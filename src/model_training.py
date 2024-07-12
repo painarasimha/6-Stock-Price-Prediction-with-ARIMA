9156f5f66d240f12483227784a21f89ba8a097fb
painarasimha <pai.narasimha04@gmail.com>
2024-07-12 20:53:29 +0530

    updates model_training.py file

--- src/model_training.py
+++ src/model_training.py
@@ -29,12 +29,12 @@ def train_model(data, order=(5,1,0)):
 
     # Plot the forecast
     forecast = model_fitted.forecast(steps=30)
-    fig7, ax7 =plt.figure(figsize=(10,6))
+    fig7, ax7 =plt.subplots(figsize=(10,6))
     ax7.plot(training_data.index, training_data, label='Historical Data')
     ax7.plot(forecast.index , forecast, label='Forecast', color='red')
-    ax7.xlabel('Date')
-    ax7.ylabel('Close Price')
-    ax7.title('Stock Price Forecast')
+    ax7.set_xlabel('Date')
+    ax7.set_ylabel('Close Price')
+    ax7.set_title('Stock Price Forecast')
     ax7.legend()
     forecasts.append(fig7)
 
